b4618e64884815aab8f31a7ab84dbfbddcbc1a24
Daniel Luque <daniel.luque@gmail.com>
2025-11-13 18:22:40 -0500

    Fix: Add TypeScript declarations for A-Frame elements

---
+++ types/aframe.d.ts
@@ -0,0 +1,17 @@
+// Type declarations for A-Frame elements
+declare namespace JSX {
+  interface IntrinsicElements {
+    'a-scene': any;
+    'a-marker': any;
+    'a-entity': any;
+    'a-camera': any;
+    'a-light': any;
+    'a-sky': any;
+    'a-box': any;
+    'a-sphere': any;
+    'a-plane': any;
+    'a-cylinder': any;
+    'a-text': any;
+  }
+}
+
